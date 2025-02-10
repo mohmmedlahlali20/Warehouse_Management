f72a197dad9bfcc752b0655dfd4849c3a9e6b6a7
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-10 21:00:50 +0100

    feat: Configure Redux store setup

--- app/(redux)/store/store.ts
+++ app/(redux)/store/store.ts
@@ -1,12 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import warehousemansSlice from '../slice/warehousemansSlice'
+import warehousemansSlice from '../slice/warehousemansSlice'; 
 
 const store = configureStore({
-    reducer: {
-        warehousemans: warehousemansSlice
-    }
-})
-
+  reducer: {
+    warehousemans: warehousemansSlice,
+  },
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
