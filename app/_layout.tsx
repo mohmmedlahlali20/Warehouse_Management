33bbc16046fb0840e58379c9f16e4dc38d11c9b8
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-10 14:58:46 +0100

    FIX: Update _layout.tsx structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,15 @@
+import React from 'react';
 import '../global.css';
+import { Provider } from "react-redux";
 
 import { Stack } from 'expo-router';
+import store from './(redux)/store/store';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <Provider store={store}>
+      <Stack/>
+    </Provider>
+
+  );
 }
