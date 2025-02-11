a83274f17183b56ca45170cff0dbfc8d6c68985f
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-11 22:06:15 +0100

    feat(product): add "productDetails.tsx" component

---
+++ app/product/productDetails.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function productDetails() {
+  return (
+    <View>
+      <Text>productDetails</Text>
+    </View>
+  )
+}
