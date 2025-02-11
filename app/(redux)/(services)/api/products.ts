48c5260c5f25a5ce806b8d5cef775aeda695e205
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-11 22:06:02 +0100

    feat(redux): update API service for "products"

--- app/(redux)/(services)/api/products.ts
+++ app/(redux)/(services)/api/products.ts
@@ -1,14 +1,23 @@
 
 
-
 const getAllProduct = async () => {
-    const res = await fetch(`${process.env.EXPO_PUBLIC_URL}/products`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-}
+    try {
+        const res = await fetch(`${process.env.EXPO_PUBLIC_URL}/products`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (!res.ok) {
+            throw new Error("Failed to fetch products");
+        }
+        return await res.json();
+    } catch (error) {
+        console.error("Error fetching products:", error);
+        throw error;
+    }
+};
+
 
 export {
     getAllProduct
