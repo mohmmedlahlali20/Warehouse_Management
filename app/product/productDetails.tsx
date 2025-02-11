dab62837d017d1d927ac84008fec2d001bc84ca7
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-11 22:44:07 +0100

    feat(product): update "productDetails.tsx" component

--- app/product/productDetails.tsx
+++ app/product/productDetails.tsx
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import React from 'react'
+import { Stack, useLocalSearchParams } from 'expo-router';
 
 export default function productDetails() {
-  return (
-    <View>
-      <Text>productDetails</Text>
-    </View>
-  )
+    const { id } = useLocalSearchParams();
+    console.log(id);
+
+
+    return (
+        <View>
+            <StatusBar barStyle="dark-content" />
+            <Stack.Screen
+                options={{
+                    headerShown: true,
+                    title: "details products X",
+                    headerStyle: { backgroundColor: "#4F46E5" },
+                    headerTintColor: "#ffffff",
+                    headerTitleStyle: { fontWeight: "bold" },
+                }}
+            />
+            <Text>productDetails</Text>
+        </View>
+    )
 }
