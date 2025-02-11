747234013efe3bd33f70dab6c47dc9137d0fe5df
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-11 16:15:35 +0100

    WM-22 feat: get all products

--- app/product/product.tsx
+++ app/product/product.tsx
@@ -1,8 +1,22 @@
 import { View, Text, StatusBar } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
+import { useAppDispatch, useAppSelector } from '~/hooks/useAppDispatch'
 
 export default function product() {
+
+    const dispatch = useAppDispatch()
+    const {isLoading, error} = useAppSelector((state) => state.Products)
+
+
+    const getProduct = async () => {
+        dispatch()
+    }
+
+
+
+
+
     return (
         <View>
             <Stack.Screen options={{ 
