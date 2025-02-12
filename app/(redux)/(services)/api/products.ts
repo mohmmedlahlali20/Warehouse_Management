67c5518a27c7cb0a894e42344c1f14eefdace824
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-12 19:49:20 +0100

    feat(redux): update API service for "products"

--- app/(redux)/(services)/api/products.ts
+++ app/(redux)/(services)/api/products.ts
@@ -2,33 +2,49 @@
 
 const getAllProduct = async () => {
 
-        const res = await fetch(`${process.env.EXPO_PUBLIC_URL}/products`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        if (!res.ok) {
-            throw new Error("Failed to fetch products");
-        }
-        return await res.json();
-    
+    const res = await fetch(`${process.env.EXPO_PUBLIC_URL}/products`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    if (!res.ok) {
+        throw new Error("Failed to fetch products");
+    }
+    return await res.json();
+
 };
 
 
 
 const getProductsById = async (productId: string) => {
-   
-    const res  = await fetch(`${process.env.EXPO_PUBLIC_URL}/products/${productId}`)
 
-    if(!res.ok){
-        throw new Error("Failed to fetch products");   
+    const res = await fetch(`${process.env.EXPO_PUBLIC_URL}/products/${productId}`)
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch products");
     }
     return await res.json()
 }
 
+const checkIfProductsExistByBarcode = async (barcode: number) => {
+
+    const res = await fetch(`${process.env.EXPO_PUBLIC_URL}/products/${barcode}`);
+
+    if (!res.ok) {
+        throw new Error(`Error: ${res.status} - ${res.statusText}`);
+    }
+
+    const product = await res.json();
+
+    return product ? true : false;
+
+};
+
+
 
 export {
     getAllProduct,
-    getProductsById
+    getProductsById,
+    checkIfProductsExistByBarcode
 }
